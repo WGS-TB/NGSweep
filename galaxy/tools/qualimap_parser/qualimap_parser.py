39c15d49f28b942c5a14caaeb60e1df573f7a05a
mtn14 <matthewnguyen.667@gmail.com>
2018-06-18 17:16:57 -0700

    Scrapped using HTML parser, manual parsing now

--- galaxy/tools/qualimap_parser/qualimap_parser.py
+++ galaxy/tools/qualimap_parser/qualimap_parser.py
@@ -1,32 +1,48 @@
 #!/usr/bin/python
 
-# Usage: python parser.py [OPTIONS] -i <report location>
+# Usage: python parser.py [OPTIONS] -i [reports]
 
 import sys
 import argparse as ap
 
-parser = ap.ArgumentParser(prog='outlier-parser', description="Parses output file to eliminate outliers")
 
+### Define global variables
+accession = ""
+mapped_percentage = ""
+mean_mapping_quality = ""
+
+parser = ap.ArgumentParser(prog='outlier-parser', conflict_handler='resolve',
+                           description="Parses output file to eliminate outliers")
 
 input = parser.add_argument_group('Input', '')
-input.add_argument('-i', '--input', required=True, metavar="STRING", help="Reports")
+input.add_argument('-i', '--input', nargs='+', required=True, help="Tab-delimited RefSeq Masher reports")
 
 if len(sys.argv) == 1:
     parser.print_usage()
     sys.exit(1)
 
 args = parser.parse_args()
+output = open("outlier_list.tsv", "a")
 
-"""Open output file"""
-output = open("outlier_list_qualimap.csv", "w")
+for file in args.input:
+    with open(file, 'r') as report:
+        iterable = iter(report)
 
-report = open(args.input)
+        for line in iterable:
+            if "BAM file:" in line and not accession:
+                line = next(iterable)
+                accession = line.split('</td>')[0].strip("<td class=column2>")
+                continue
 
-for line in report:
-    if "number of mapped reads" in line:
-        mapped_percentage = line.split()[-1].strip('()%')
-    if "mean mapping quality" in line:
-        mean_mapping_quality = line.split()[-1]
+            if "Mapped reads" in line and not mapped_percentage:
+                line = next(iterable)
+                mapped_percentage = line.split('</td')[0].strip("<td class=column2>").split()[2].strip("%")
+                continue
 
-    if float(mapped_percentage) < 90 or float(mean_mapping_quality) < 10:
-        outlier_flag = True
+            if "Mean Mapping Quality" in line and not mean_mapping_quality:
+                line = next(iterable)
+                mean_mapping_quality = line.split('</td>')[0].strip("<td class=column2>")
+                break
+
+        if float(mapped_percentage) < 90 or float(mean_mapping_quality) < 10:
+            output.write("%s\n" % accession[:accession.find(".")])
